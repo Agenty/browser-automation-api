35de9d20c479b1b897eb361e9a1a50da0bdd41b2
Vikash Rathee <vickyrathee@users.noreply.github.com>
2020-05-13 12:09:49 +0530

    capture screenshot example

---
+++ function/3-capture-screenshot.js
@@ -0,0 +1,14 @@
+
+// Read the `url` from request, goto the page, capture screenshot and return the results
+
+module.exports = async ({ page, request }) => {
+    const response = await page.goto('https:/example.com/');  
+    console.log(`statusCode : ${response.status()}`);
+    
+    await page.screenshot({path : 'example.png'});
+    
+    return {
+        data : {},
+        type : 'application/json'
+    };   
+};
