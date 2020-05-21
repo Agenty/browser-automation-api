01d87f723b0ad395a638034776890d20425285ca
Vikash Rathee <vickyrathee@users.noreply.github.com>
2020-05-21 15:40:44 +0530

    paginateAndScrape re-written

--- function/4-pagination-and-scrape.js
+++ function/4-pagination-and-scrape.js
@@ -2,16 +2,7 @@
 // click on next button and repeat until maxPages, then return the results
 
 module.exports = async ({ page, request }) => {
-    console.log('Navigating...');
-    await page.goto(request.url);
-
-    const maxPages = request.maxPages || 3;
-    const result = [];
-    
-    for(var p = 0; p < maxPages; p++){
-      
-     // Extract page content 
-     const pageResult = await page.evaluate(() => {
+    function extractProducts() {
         const data = [];
         var products = document.querySelectorAll('.product_pod');
         for (var product of products) {
@@ -23,19 +14,31 @@ module.exports = async ({ page, request }) => {
                 product_link: product.querySelector('h3 > a').getAttribute("href")
             });
         }
-            return data;
-      });
-      
-      console.log(`Found ${pageResult.length} products on page ${p}`);
-      
-      // push page result to outer array;
-      result.push(...pageResult);
-      
-      // Click on next button
-      console.log('Click next...');
-      await page.click('.next a');
-
+        return data;
     }
+    
+    async function paginateAndScrape(page) {
+        const result = [];
+        for(var p = 0; p < maxPages; p++){
+          let products = await page.evaluate(extractProducts);
+          console.log(`Found ${products.length} products on page ${p}`);
+          
+          // push products result array;
+          result.push(...products);
+          
+          // Click on next button
+          console.log('Click next...');
+          await page.click('.next a');
+          await sleep(1000);
+        }
+        return result;
+    }
+    
+    console.log('Navigating...');
+    await page.goto(request.url);
+
+    const maxPages = request.maxPages || 3;
+    const result  = await paginateAndScrape(page);
     
     return {
         data: result,
